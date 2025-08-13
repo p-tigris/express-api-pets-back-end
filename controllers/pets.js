3e13e71bf76482966560d98a915c10af7ad13ed1
smdhar <shoumik_dhar@yahoo.com>
2025-08-13 13:07:16 -0400

    Implemented delete and update routes

--- controllers/pets.js
+++ controllers/pets.js
@@ -43,4 +43,48 @@ router.get("/:petId", async (req, res) => {
     }
 })
 
+// DELETE
+router.delete("/:petId", async (req, res) => {
+    try {
+        const foundPet = await Pet.findByIdAndDelete(req.params.petId);
+
+        if (!foundPet) {
+            res.status(404);
+            throw new Error('Pet not found');
+        }
+
+        res.status(200).json(foundPet);
+    } catch (error) {
+        
+        if (res.statusCode === 404) {
+            res.json({ error: error.message });
+        } else {
+            res.status(500).json({ error: error.message });
+        }
+
+    }
+})
+
+// UPDATE - PUT - /pets/:petId
+router.put('/:petId', async (req, res) => {
+  try {
+    const updatedPet = await Pet.findByIdAndUpdate(req.params.petId, req.body, { new: true });
+
+    if (!updatedPet) {
+        res.status(404);
+        throw new Error('Pet not found');
+    }
+
+    res.status(200).json(updatedPet);
+  } catch (error) {
+
+        if (res.statusCode === 404) {
+            res.json({ error: error.message });
+        } else {
+            res.status(500).json({ error: error.message });
+        }
+  }
+});
+
+
 module.exports = router;
